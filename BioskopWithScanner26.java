71a986a7dc49e8fa18a0159729791d7c21c1d05f
rochiszigmakinara <rochiszigmakinara@gmail.com>
2025-11-12 21:28:27 +0700

    pertanyaan 2

--- BioskopWithScanner26.java
+++ BioskopWithScanner26.java
@@ -3,30 +3,66 @@ import java.util.Scanner;
 public class BioskopWithScanner26 {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
-        int baris, kolom;
+        int baris, kolom, menu;
         String nama, next;
         String [][] penonton= new String [4][2];
 
-        while (true) {
-            System.out.print("Masukkan nama: ");
-            nama= sc.nextLine();
-            System.out.print("Masukkan baris: ");
-            baris= sc.nextInt();
-            System.out.print("Masukkan kolom: ");
-            kolom= sc.nextInt();
-            sc.nextLine();
+        System.out.println("=======BIOSKOP 26=======");
+        System.out.println("Menu 1: pilih kursi");
+        System.out.println("Menu 2: daftar penonton");
+        System.out.println("Menu 3: exit");
+        System.out.println("========================");
+        System.out.print("\nPilih menu: ");
+        menu= sc.nextInt();
+        sc.nextLine();
 
-            penonton[baris-1][kolom-1]= nama;
+        System.out.println("------------------------");
+        switch(menu) {
+            case 1:
+                while (true) {
+                    System.out.print("Masukkan nama: ");
+                    nama= sc.nextLine();
+                    System.out.print("Masukkan baris: ");
+                    baris= sc.nextInt();
+                    System.out.print("Masukkan kolom: ");
+                    kolom= sc.nextInt();
+                    sc.nextLine();
 
-            System.out.print("Input penonton lainnya? (Y/N): ");;
-            next= sc.nextLine();
+                    penonton[baris-1][kolom-1]= nama;
 
-            if (next.equalsIgnoreCase("n")) {
-                break;
-            }
+                    System.out.print("Input penonton lainnya? (Y/N): ");;
+                    next= sc.nextLine();
+                        if (next.equalsIgnoreCase("n")) {
+                            break;
+                        }
+                    
+                }
 
-            
+            case 2:
+            System.out.println("\n=========Daftar Penonton=========");
+                for (int i = 0; i < penonton.length; i++) {
+                        System.out.print((i + 1)+"  ");
+                        for (int j = 0; j < penonton[i].length; j++) {
+                            if (penonton[i][j] == null) {
+                                System.out.print("*");
+                            } else {
+                                System.out.print(penonton[i][j]);
+                            }
+                            if (j < penonton[i].length - 1) {
+                                System.out.print("\t\t");
+                            }
+                        }
+                        System.out.println();
+                    }
+                    break;
+
+            case 3:
+            System.out.println("EXIT");
+            return;
         }
+        
+            
+        
 
     }
 }
