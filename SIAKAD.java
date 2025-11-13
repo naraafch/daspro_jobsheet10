fe46a612beaf1e6e8922667e50b2ebccaaadeabd
rochiszigmakinara <rochiszigmakinara@gmail.com>
2025-11-13 09:17:48 +0700

    poercobaan 3

--- SIAKAD.java
+++ SIAKAD.java
@@ -7,13 +7,30 @@ public class SIAKAD {
 
         for (int i=0; i<nilai.length; i++) {
             System.out.println("Input nilai mahasiswa ke-" +(i+1));
+            double totalPerSiswa= 0;
 
             for (int j=0; j<nilai[1].length; j++) {
                 System.out.print("Nilai mata kuliah " +(j+1)+ ": ");
                 nilai[i][j]= sc.nextInt();
-
+                totalPerSiswa+=nilai[i][j];
             }
+
+            System.out.println("Nilai rata-rata: " +totalPerSiswa/3);
         }
+
+        System.out.println("\n======================================");
+        System.out.println("Rata-rata nilai setiap mata kuliah: ");;
+
+        for (int j=0; j<3; j++) {
+            double totalPerMatkul=0;
+
+            for (int i=0; i<4; i++) {
+                totalPerMatkul+=nilai[i][j];
+            }
+
+            System.out.println("Mata kuliah " +(j+1)+ ": " +totalPerMatkul/4);
+        }
+    
     }
     
 }
